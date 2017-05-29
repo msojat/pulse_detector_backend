aad6ba5885b4b0760fdc6de7c1ce86d47ba41e1c
Denis Fodor <denis@arsfutura.co>
2017-05-29 20:00:26 +0200

    added record posting

--- database_actions.js
+++ database_actions.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var Guid = require('guid');
 var model = require("./model/database_model");
+var config = require("../config.json");
 var router = express.Router();
 
 router.post("/add_user", function (req, res) {
 
-    if (!req.body.name || !req.body.surname || !req.body.jmbag || !req.body.record_num || !req.body.record_length) {
+    if (!req.body.name || !req.body.surname || !req.body.jmbag || !req.body.number_of_records ||
+        req.body.app_secret !== config.app_secret) {
         res.status(400).send();
         return;
     }
@@ -23,7 +25,6 @@ router.post("/add_user", function (req, res) {
             name: req.body.name,
             surname: req.body.surname,
             jmbag: req.body.jmbag,
-            role_id: 2, //regular user constant
             last_access_date: new Date().toISOString().slice(0, 19).replace('T', ' ') //set current time
         });
 
@@ -36,7 +37,7 @@ router.post("/add_user", function (req, res) {
         var guid = Guid.create();
         var identifier = new Identifier({
             identifier: guid,
-            number_of_records: req.body.record_num
+            number_of_records: req.body.number_of_records
         });
 
         user.save(function (err, result) {
@@ -44,12 +45,16 @@ router.post("/add_user", function (req, res) {
                 res.status(500).send(err);
             }
 
+            if (id === -1) {
+                id = result.insertId;
+            }
+
             identifier.save(function (error, r) {
                 if (error) {
                     res.status(500).send(error);
                 }
 
-                res.json({"identifier_id": r.insertId});
+                res.json({"identifier_id": r.insertId, "user_id": id});
             });
 
         });
@@ -63,6 +68,50 @@ router.get("/get_users", function (req, res) {
     user.find("all", {}, function (err, rows, fields) {
         res.json(rows);
     })
+});
+
+router.post("/add_record", function (req, res) {
+
+    if (!req.body.user_id || !req.body.identifier_id || !req.body.record_length || !req.body.record_number ||
+        !req.body.start_record_time || !req.body.end_record_time || !req.body.heart_rate ||
+        req.body.app_secret !== config.app_secret) {
+        res.status(400).send();
+        return;
+    }
+
+    var Record = model.record;
+
+    var record = new Record({
+        user_id: req.body.user_id,
+        record_length: req.body.record_length,
+        identifier_id: req.body.identifier_id,
+        record_number: req.body.record_number,
+        start_record_time: req.body.start_record_time,
+        end_record_time: req.body.end_record_time
+    });
+
+    record.save(function (err, result) {
+        if (err) {
+            res.status(500).send(err);
+        }
+
+        var HeartRate = model.heartRate;
+
+        var heartRate = new HeartRate({
+            heart_rate: req.body.heart_rate,
+            identifier_id: req.body.identifier_id,
+            record_id: result.insertId
+        });
+
+        heartRate.save(function (err, result) {
+            if (err) {
+                res.status(500).send(err);
+            }
+
+            res.status(204).end();
+
+        });
+    });
 });
 
 module.exports = router;
